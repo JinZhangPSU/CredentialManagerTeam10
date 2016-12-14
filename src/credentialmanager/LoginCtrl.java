6d51f0c8f33e351b4b3f4877f60e16981c3379c0
Jin <zhangjin94@gmail.com>
2016-12-13 19:39:37 -0500

    add error message when wrong

--- src/credentialmanager/LoginCtrl.java
+++ src/credentialmanager/LoginCtrl.java
@@ -9,6 +9,10 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
 import javax.swing.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
@@ -19,15 +23,41 @@ public class LoginCtrl {
     static JFrame frame = new JFrame("Credential Manager (Login)");
 
     public static void OpenHomePage(String name, String password) throws GeneralSecurityException, UnsupportedEncodingException, IOException {
-        User u = new User("a", "b", name, password);
-        if (name.compareTo("zhangjin") == 0 && password.compareTo("123") == 0) {
-            
-            AESCrypt aes = new AESCrypt(true, "pass");
-            aes.decrypt("src/zhangjin.txt","src/tempFile.txt");
-            CredentialManager.openHomePage(u);
-            frame.dispose();
-        }
+         Scanner fileScan = new Scanner(new File("src/usersDB.txt"));
+         User u = new User("a", "b", name, password);
+         if(!name.equals("")&&!password.equals(""))
+         {
+         while (fileScan.hasNextLine()){
+            String  username = fileScan.nextLine();
+            if(username.equals(name))
+            {
+                while (fileScan.hasNextLine()){
+                    String userpassword = fileScan.nextLine();
+                    if(userpassword.equals(password)){
+                    AESCrypt aes = new AESCrypt(true, "pass");
+                    aes.decrypt("src/zhangjin.txt","src/tempFile.txt");
+                    CredentialManager.openHomePage(u);
+                    frame.dispose();
+                    break;
+                    }
+                    else if(fileScan.hasNextLine()==false) {
+                   JOptionPane.showMessageDialog(null,
+                   "Invalid password. Try again.",
+                   "Error Message",
+                   JOptionPane.ERROR_MESSAGE);
+                    }
+                }             
+            }  
 
+         }
+       
+         }
+            else{
+                   JOptionPane.showMessageDialog(null,
+                   "Both field must be entered!",
+                   "Error Message",
+                   JOptionPane.ERROR_MESSAGE);
+            }
     }
 
     public static void OpenSignUpPage() {
